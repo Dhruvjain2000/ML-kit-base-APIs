169ea33b2dfc31a15e24eb41ffd38b441ab2651f
Dhruv Jain <dhruvjain2212000@gmail.com>
2018-10-01 14:35:25 +0530

    Face recognition added

--- app/src/main/java/com/example/android/mlkitbaseapis/FaceDetection.java
+++ app/src/main/java/com/example/android/mlkitbaseapis/FaceDetection.java
@@ -1,13 +1,147 @@
 package com.example.android.mlkitbaseapis;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark;
+
+import java.util.List;
 
 public class FaceDetection extends AppCompatActivity {
 
+    private FirebaseVisionFaceDetectorOptions options ;
+    private ImageView mImageView;
+    private TextView txtView;
+    private Bitmap imageBitmap;
+    private Button snapBtn;
+    private Button detectBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_detection);
+
+        mImageView = findViewById(R.id.imageView);
+        txtView = findViewById(R.id.txtView);
+        snapBtn = findViewById(R.id.snapBtn);
+        detectBtn = findViewById(R.id.detectBtn);
+
+         options = new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setModeType(FirebaseVisionFaceDetectorOptions.ACCURATE_MODE)
+                        .setLandmarkType(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .setMinFaceSize(0.15f)
+                        .setTrackingEnabled(true)
+                        .build();
+
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectImage();
+            }
+        });
+    }
+
+    private void detectImage() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector(options);
+
+        Task<List<FirebaseVisionFace>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionFace> faces) {
+                                        processImage(faces);
+                                        
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getBaseContext(), e.getMessage() , Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+    }
+
+    private void processImage(List<FirebaseVisionFace> faces) {
+        for (FirebaseVisionFace face : faces) {
+            Rect bounds = face.getBoundingBox();
+            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
+            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
+
+            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+            // nose available):
+            FirebaseVisionFaceLandmark leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR);
+            if (leftEar != null) {
+                FirebaseVisionPoint leftEarPos = leftEar.getPosition();
+            }
+
+            // If classification was enabled:
+            float smileProb = 0 ;
+            if (face.getSmilingProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                   smileProb = face.getSmilingProbability();
+            }
+            float rightEyeOpenProb = 0;
+            if (face.getRightEyeOpenProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                  rightEyeOpenProb = face.getRightEyeOpenProbability();
+            }
+
+            // If face tracking was enabled:
+            if (face.getTrackingId() != FirebaseVisionFace.INVALID_ID) {
+                int id = face.getTrackingId();
+            }
+
+            String result  = "Position of head : Y rotation :" + rotY + "Z rotation : " + rotZ +"Ear Position"+ leftEar + "Smile Probablity :" + smileProb + "Eye open probablity : "+rightEyeOpenProb;
+                    txtView.setText(result);
+        }
+
+
+    }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
